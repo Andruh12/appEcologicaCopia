cf47a709c9ed4197949ddbb12644119e77c89dc6
Stiven327 <yspereza@sanmateo.edu.co>
2024-10-14 21:00:12 -0500

    Add files via upload

---
+++ main/java/com/example/app1/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app1
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)  // Asegúrate de que el nombre del layout sea correcto
+    }
+}
