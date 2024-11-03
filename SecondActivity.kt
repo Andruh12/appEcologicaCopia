a79314464480db74abc82a4d251ad208967d8b43
Stiven327 <yspereza@sanmateo.edu.co>
2024-11-03 15:06:47 -0500

    SecondActivity.kt

--- SecondActivity.kt
+++ SecondActivity.kt
@@ -1,12 +1,20 @@
 package com.example.app1
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)  // Asegúrate de que el nombre del layout sea correcto
+        setContentView(R.layout.activity_second)
+
+        val textOlvidasteContrasena = findViewById<TextView>(R.id.textOlvidasteContrasena)
+        textOlvidasteContrasena.setOnClickListener {
+
+            val intent = Intent(this, FourActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
